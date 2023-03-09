2fd00bc03b53249651956878e0e3efda6341db56
SaNjaYsinGh1123 <sanjayfbd93544@gmail.com>
2023-03-09 13:13:34 +0530

    try 2

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const db = require('./config/mongoose');
 const path = require('path');
 const multer = require('multer');
+const cors = require('cors');
 app.use(express.json());
+app.use(cors());
 
 app.use("/images", express.static(path.join(__dirname,'/images')));
 
