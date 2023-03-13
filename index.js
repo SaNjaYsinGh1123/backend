d5273808ce0649da766e0926a6d5c39a9ac7aa40
SaNjaYsinGh1123 <sanjayfbd93544@gmail.com>
2023-03-13 17:26:36 +0530

    try 11

--- index.js
+++ index.js
@@ -20,22 +20,22 @@ app.use(bodyParser.urlencoded({
 app.use(cors());
 app.use("/images", express.static(path.join(__dirname,'/images')));
 
-const storage = multer.diskStorage({
-    destination: (req,file,callback)=>{
-        callback(null,"images")
-    },
-    filename: (req,file,callback)=>{
-        callback(null, file.originalname)
-    },
-})
+// const storage = multer.diskStorage({
+//     destination: (req,file,callback)=>{
+//         callback(null,"images")
+//     },
+//     filename: (req,file,callback)=>{
+//         callback(null, file.originalname)
+//     },
+// })
 
 
-const upload = multer({storage: storage});
+// const upload = multer({storage: storage});
 
 
-app.post('/upload',upload.single('file'), (req,res)=>{
-    res.status(200).json('file has been uploaded');
-})
+// app.post('/upload',upload.single('file'), (req,res)=>{
+//     res.status(200).json('file has been uploaded');
+// })
 app.use('/',require('./routes'));
 app.listen(process.env.PORT || 3000,()=>{
     console.log('backend running')
